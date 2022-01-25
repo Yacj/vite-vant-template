fb5642c1c0415fdd998383497b2b39d690550341
yacj <1356888297@qq.com>
2022-01-25 09:29:26 +0800

    update:更新依赖 fixBug:修复没有引入样式问题

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import router from "./router";
 import pinIaStore from "./store";
+import './styles/base.scss'
 
 const app = createApp(App)
 app.use(router)

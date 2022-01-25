64739890b71079d8833c5d1f026aee7ae2f7ce07
yacj <1356888297@qq.com>
2022-01-25 16:27:16 +0800

    update:优化有无登录逻辑

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,37 @@
-import {createRouter, createWebHashHistory} from 'vue-router'
-import {getToken} from "../utils/auth";
+import { Toast } from "vant";
+import { createRouter, createWebHashHistory } from "vue-router";
+import { isLogin } from "../utils/auth";
 
 const routes = [
-    {
-        path: '/',
-        redirect: '/home'
+  {
+    path: "/",
+    redirect: "/home",
+  },
+  {
+    path: "/home",
+    name: "home",
+    component: () => import("../views/home/index.vue"),
+    meta: {
+      title: "首页",
+      keepAlive: true,
     },
-    {
-        path: '/home',
-        name: 'home',
-        component: () => import('../views/home/index.vue'),
-        meta: {
-            title: '首页',
-            keepAlive: true
-        }
-    }
-]
+  },
+];
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes
-})
+  history: createWebHashHistory(),
+  routes,
+});
 router.beforeEach((to, form, next) => {
-    const {title, isLogin} = to.meta
-    const token = getToken()
-    if (title) {
-        document.title = title
-    }
-    if (isLogin && !token) {
-        next('login')
-        return
-    }
-    next()
-})
-export default router
+  const { title, needLogin } = to.meta;
+  if (title) {
+    document.title = title;
+  }
+  if (needLogin && !isLogin()) {
+    Toast.fail("暂未登录，请登录后再世");
+    next("login");
+    return;
+  }
+  next();
+});
+export default router;
